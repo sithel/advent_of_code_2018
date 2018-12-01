2c1d7743f20ad52b62ba8fc12663c6867c33f08c
Rebecca S <rebecca.stecker@gmail.com>
2018-12-01 11:31:00 -0800

    done with part 2

--- app/src/test/java/app/sixarmstudios/adventofcode2018/Day1.kt
+++ app/src/test/java/app/sixarmstudios/adventofcode2018/Day1.kt
@@ -17,12 +17,13 @@ import java.io.InputStreamReader
  * Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
  *
  * Starting at 10:42am 12/1/2018
+ * Starting part 2 at 11:09am 12/1/2018
  */
 class Day1 {
     private fun solve_part1(input: String): Int {
         return input.replace(",", "")
                 .replace("+", "")
-                .replace("\n"," ")
+                .replace("\n", " ")
                 .split(" ")
                 .filter { it.isNotBlank() }
                 .map { it.toInt() }
@@ -47,10 +48,66 @@ class Day1 {
     @Test
     fun input_part1() {
         val inputStream = this.javaClass.classLoader!!.getResourceAsStream("day1_input.txt")
-        val inputStr = BufferedReader( InputStreamReader(inputStream)).readText()
+        val inputStr = BufferedReader(InputStreamReader(inputStream)).readText()
         inputStream.close()
         assertThat(solve_part1(inputStr), `is`(420))
         // 11:07am, first gold star
+    }
+
+    private fun solve_part2(input: String): Int {
+        val list = input.replace(",", "")
+                .replace("+", "")
+                .replace("\n", " ")
+                .split(" ")
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+
+        return findFreq(0, list, mutableSetOf(0))
+    }
+
+    private fun findFreq(startValue: Int, list: List<Int>, pastFreq: MutableSet<Int>): Int {
+        val endValue = list.fold(startValue) { prevValue, acc ->
+            val newFreq = prevValue + acc
+            if (pastFreq.contains(newFreq))
+                return newFreq  // bail HARD out of the function, we're done!
+            pastFreq.add(newFreq)
+            newFreq
+        }
+        return findFreq(endValue, list, pastFreq)
+    }
+
+    @Test
+    fun sample1_part2() {
+        assertThat(solve_part2("+1, -1"), `is`(0))
+    }
+
+    @Test
+    fun sample2_part2() {
+        assertThat(solve_part2("+3, +3, +4, -2, -4"), `is`(10))
+    }
+
+    @Test
+    fun sample3_part2() {
+        assertThat(solve_part2("-6, +3, +8, +5, -6"), `is`(5))
+    }
+
+    @Test
+    fun sample4_part2() {
+        assertThat(solve_part2("+7, +7, -2, -7, -4"), `is`(14))
+    }
+
+    @Test
+    fun sample0_part2() {
+        assertThat(solve_part2("+1, -2, +3, +1"), `is`(2))
+    }
+
+    @Test
+    fun input_part2() {
+        val inputStream = this.javaClass.classLoader!!.getResourceAsStream("day1_input.txt")
+        val inputStr = BufferedReader(InputStreamReader(inputStream)).readText()
+        inputStream.close()
+        assertThat(solve_part2(inputStr), `is`(227))
+        // done at 11:30am
     }
 
 
